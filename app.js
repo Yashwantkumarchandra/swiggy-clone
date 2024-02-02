7b5efa172867d89ed592e812dc6c781f72749e42
yashwantkumarchandra <yashwantchandra904@gmail.com>
2024-02-02 14:09:33 +0530

    using hardCode data

--- app.js
+++ app.js
@@ -1,1 +1,748 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+
+
+const resData =  [
+  {
+  "info": {
+  "id": "533383",
+  "name": "Domino's Pizza",
+  "cloudinaryImageId": "jjzlk1ikevc2w76jphiy",
+  "locality": "Deepthi Srinagar Colony",
+  "areaName": "Miyapur",
+  "costForTwo": "₹400 for two",
+  "cuisines": [
+  "Pizzas",
+  "Italian",
+  "Pastas",
+  "Desserts"
+  ],
+  "avgRating": 4.2,
+  "parentId": "2456",
+  "avgRatingString": "4.2",
+  "totalRatingsString": "1K+",
+  "sla": {
+  "deliveryTime": 30,
+  "serviceability": "SERVICEABLE",
+  "slaString": "30 mins",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-02-01 02:59:00",
+  "opened": true
+  },
+  "badges": {},
+  "isOpen": true,
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {},
+  "textBased": {},
+  "textExtendedBadges": {}
+  }
+  },
+  "aggregatedDiscountInfoV3": {
+  "header": "₹150 OFF",
+  "subHeader": "ABOVE ₹299",
+  "discountTag": "FLAT DEAL"
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {},
+  "cta": {},
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+  "info": {
+  "id": "755854",
+  "name": "Leon's - Burgers & Wings (Leon Grill)",
+  "cloudinaryImageId": "b2edbc28b7b8219d6e0a9c049ce06658",
+  "locality": "Sri Rangapuram Colony",
+  "areaName": "Miyapur",
+  "costForTwo": "₹300 for two",
+  "cuisines": [
+  "American",
+  "Fast Food"
+  ],
+  "avgRating": 4.3,
+  "parentId": "371281",
+  "avgRatingString": "4.3",
+  "totalRatingsString": "500+",
+  "sla": {
+  "deliveryTime": 31,
+  "lastMileTravel": 6,
+  "serviceability": "SERVICEABLE",
+  "slaString": "30-35 mins",
+  "lastMileTravelString": "6.0 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-02-01 03:00:00",
+  "opened": true
+  },
+  "badges": {},
+  "isOpen": true,
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {},
+  "textBased": {},
+  "textExtendedBadges": {}
+  }
+  },
+  "aggregatedDiscountInfoV3": {
+  "header": "50% OFF",
+  "subHeader": "UPTO ₹100"
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "isNewlyOnboarded": true,
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {
+  "context": "seo-data-baeec0b1-b6ff-4ed6-ab7b-f495f510f448"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/restaurants/leons-burgers-and-wings-leon-grill-sri-rangapuram-colony-miyapur-hyderabad-755854",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+  "info": {
+  "id": "577939",
+  "name": "Burger King",
+  "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+  "locality": "Chandanagar Circle No 21",
+  "areaName": "Miyapur",
+  "costForTwo": "₹350 for two",
+  "cuisines": [
+  "Burgers",
+  "American"
+  ],
+  "avgRating": 4.1,
+  "parentId": "166",
+  "avgRatingString": "4.1",
+  "totalRatingsString": "5K+",
+  "sla": {
+  "deliveryTime": 29,
+  "lastMileTravel": 4.5,
+  "serviceability": "SERVICEABLE",
+  "slaString": "25-30 mins",
+  "lastMileTravelString": "4.5 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-02-01 03:00:00",
+  "opened": true
+  },
+  "badges": {},
+  "isOpen": true,
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {},
+  "textBased": {},
+  "textExtendedBadges": {}
+  }
+  },
+  "aggregatedDiscountInfoV3": {
+  "header": "ITEMS",
+  "subHeader": "AT ₹129"
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {
+  "context": "seo-data-baeec0b1-b6ff-4ed6-ab7b-f495f510f448"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/restaurants/burger-king-chandanagar-circle-no-21-miyapur-hyderabad-577939",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+  "info": {
+  "id": "808213",
+  "name": "MOJO Pizza - 2X Toppings",
+  "cloudinaryImageId": "1a24dadf4ef47acd7f30911c3ae6f4de",
+  "locality": "SAI PRUTHIVI ENCLAVE",
+  "areaName": "KONDAPUR",
+  "costForTwo": "₹250 for two",
+  "cuisines": [
+  "Pizzas",
+  "Italian",
+  "Fast Food",
+  "Desserts"
+  ],
+  "avgRating": 4.5,
+  "parentId": "11329",
+  "avgRatingString": "4.5",
+  "totalRatingsString": "20+",
+  "sla": {
+  "deliveryTime": 22,
+  "lastMileTravel": 3.9,
+  "serviceability": "SERVICEABLE",
+  "slaString": "17-27 mins",
+  "lastMileTravelString": "3.9 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-02-01 02:00:00",
+  "opened": true
+  },
+  "badges": {},
+  "isOpen": true,
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {},
+  "textBased": {},
+  "textExtendedBadges": {}
+  }
+  },
+  "aggregatedDiscountInfoV3": {
+  "header": "50% OFF",
+  "subHeader": "UPTO ₹100"
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "isNewlyOnboarded": true,
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {
+  "context": "seo-data-baeec0b1-b6ff-4ed6-ab7b-f495f510f448"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/restaurants/mojo-pizza-2x-toppings-sai-pruthivi-enclave-kondapur-hyderabad-808213",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+  "info": {
+  "id": "344923",
+  "name": "Baskin Robbins - Ice Cream Desserts",
+  "cloudinaryImageId": "85ccae4e3576f9330af102c46ca85395",
+  "locality": "Sri Ram Nagar, Kondapur",
+  "areaName": "Sri Ram Nagar",
+  "costForTwo": "₹250 for two",
+  "cuisines": [
+  "Desserts"
+  ],
+  "avgRating": 4.4,
+  "veg": true,
+  "parentId": "5588",
+  "avgRatingString": "4.4",
+  "totalRatingsString": "1K+",
+  "sla": {
+  "deliveryTime": 24,
+  "lastMileTravel": 3.9,
+  "serviceability": "SERVICEABLE",
+  "slaString": "20-25 mins",
+  "lastMileTravelString": "3.9 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-02-01 05:00:00",
+  "opened": true
+  },
+  "badges": {
+  "imageBadges": [
+  {
+  "imageId": "v1695133679/badges/Pure_Veg111.png",
+  "description": "pureveg"
+  }
+  ]
+  },
+  "isOpen": true,
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {
+  "badgeObject": [
+  {
+  "attributes": {
+  "description": "pureveg",
+  "imageId": "v1695133679/badges/Pure_Veg111.png"
+  }
+  }
+  ]
+  },
+  "textBased": {},
+  "textExtendedBadges": {}
+  }
+  },
+  "aggregatedDiscountInfoV3": {
+  "header": "30% OFF",
+  "subHeader": "UPTO ₹75"
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {
+  "context": "seo-data-baeec0b1-b6ff-4ed6-ab7b-f495f510f448"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/restaurants/baskin-robbins-ice-cream-desserts-kondapur-sri-ram-nagar-hyderabad-344923",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+  "info": {
+  "id": "449668",
+  "name": "Premium thick shakes",
+  "cloudinaryImageId": "p7egb8grou45d0djc9pi",
+  "locality": "Serlingampally Circle No 20",
+  "areaName": "kondapur",
+  "costForTwo": "₹258 for two",
+  "cuisines": [
+  "Beverages"
+  ],
+  "avgRating": 4.3,
+  "veg": true,
+  "parentId": "270475",
+  "avgRatingString": "4.3",
+  "totalRatingsString": "1K+",
+  "sla": {
+  "deliveryTime": 21,
+  "lastMileTravel": 3.9,
+  "serviceability": "SERVICEABLE",
+  "slaString": "20-25 mins",
+  "lastMileTravelString": "3.9 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-02-01 23:59:00",
+  "opened": true
+  },
+  "badges": {
+  "imageBadges": [
+  {
+  "imageId": "v1695133679/badges/Pure_Veg111.png",
+  "description": "pureveg"
+  }
+  ]
+  },
+  "isOpen": true,
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {
+  "badgeObject": [
+  {
+  "attributes": {
+  "description": "pureveg",
+  "imageId": "v1695133679/badges/Pure_Veg111.png"
+  }
+  }
+  ]
+  },
+  "textBased": {},
+  "textExtendedBadges": {}
+  }
+  },
+  "aggregatedDiscountInfoV3": {
+  "header": "50% OFF",
+  "discountTag": "FLAT DEAL"
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {
+  "context": "seo-data-baeec0b1-b6ff-4ed6-ab7b-f495f510f448"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/restaurants/premium-thick-shakes-serlingampally-circle-no-20-kondapur-hyderabad-449668",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+  "info": {
+  "id": "733528",
+  "name": "Bismi Shawarma",
+  "cloudinaryImageId": "3dff19c6783e3d25d34100bba0af3991",
+  "locality": "A P H B colony",
+  "areaName": "Gachibowli",
+  "costForTwo": "₹250 for two",
+  "cuisines": [
+  "Snacks"
+  ],
+  "avgRating": 3.6,
+  "parentId": "435463",
+  "avgRatingString": "3.6",
+  "totalRatingsString": "500+",
+  "sla": {
+  "deliveryTime": 27,
+  "lastMileTravel": 6.9,
+  "serviceability": "SERVICEABLE",
+  "slaString": "25-30 mins",
+  "lastMileTravelString": "6.9 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-02-07 00:00:00",
+  "opened": true
+  },
+  "badges": {},
+  "isOpen": true,
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {},
+  "textBased": {},
+  "textExtendedBadges": {}
+  }
+  },
+  "aggregatedDiscountInfoV3": {
+  "header": "50% OFF",
+  "subHeader": "UPTO ₹100"
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {
+  "context": "seo-data-baeec0b1-b6ff-4ed6-ab7b-f495f510f448"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/restaurants/bismi-shawarma-a-p-h-b-colony-gachibowli-hyderabad-733528",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+  "info": {
+  "id": "280578",
+  "name": "Chickpet Donne Biryani House Miyapur",
+  "cloudinaryImageId": "a6459c8bb2f03fb60381f920619a23f5",
+  "locality": "Miyapur",
+  "areaName": "Indira Nagar Colony",
+  "costForTwo": "₹400 for two",
+  "cuisines": [
+  "Biryani"
+  ],
+  "avgRating": 4,
+  "parentId": "19396",
+  "avgRatingString": "4.0",
+  "totalRatingsString": "5K+",
+  "sla": {
+  "deliveryTime": 26,
+  "lastMileTravel": 4.2,
+  "serviceability": "SERVICEABLE",
+  "slaString": "25-30 mins",
+  "lastMileTravelString": "4.2 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-02-01 01:00:00",
+  "opened": true
+  },
+  "badges": {},
+  "isOpen": true,
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {},
+  "textBased": {},
+  "textExtendedBadges": {}
+  }
+  },
+  "aggregatedDiscountInfoV3": {
+  "header": "40% OFF",
+  "subHeader": "UPTO ₹80"
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {
+  "context": "seo-data-baeec0b1-b6ff-4ed6-ab7b-f495f510f448"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/restaurants/chickpet-donne-biryani-house-miyapur-miyapur-indira-nagar-colony-hyderabad-280578",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+  "info": {
+  "id": "64092",
+  "name": "Hanu King's Burger",
+  "cloudinaryImageId": "vggaoogsd6c6z6naoffi",
+  "locality": "Kondapur X Road",
+  "areaName": "Hanuman Nagar",
+  "costForTwo": "₹350 for two",
+  "cuisines": [
+  "Fast Food",
+  "Indian",
+  "Snacks",
+  "Continental",
+  "American"
+  ],
+  "avgRating": 4.2,
+  "parentId": "93295",
+  "avgRatingString": "4.2",
+  "totalRatingsString": "5K+",
+  "sla": {
+  "deliveryTime": 31,
+  "lastMileTravel": 5,
+  "serviceability": "SERVICEABLE",
+  "slaString": "30-35 mins",
+  "lastMileTravelString": "5.0 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-02-01 05:45:00",
+  "opened": true
+  },
+  "badges": {
+  "textExtendedBadges": [
+  {
+  "iconId": "guiltfree/GF_Logo_android_3x",
+  "shortDescription": "options available",
+  "fontColor": "#7E808C"
+  }
+  ]
+  },
+  "isOpen": true,
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {},
+  "textBased": {},
+  "textExtendedBadges": {
+  "badgeObject": [
+  {
+  "attributes": {
+  "description": "",
+  "fontColor": "#7E808C",
+  "iconId": "guiltfree/GF_Logo_android_3x",
+  "shortDescription": "options available"
+  }
+  }
+  ]
+  }
+  }
+  },
+  "aggregatedDiscountInfoV3": {
+  "header": "20% OFF",
+  "subHeader": "UPTO ₹50"
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {
+  "context": "seo-data-baeec0b1-b6ff-4ed6-ab7b-f495f510f448"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/restaurants/hanu-kings-burger-kondapur-x-road-hanuman-nagar-hyderabad-64092",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  }
+  ]
+
+const Header = () =>{
+  return(
+    <div className="header">
+      <div className="logo"><img src="https://img.freepik.com/free-vector/detailed-chef-logo-template_23-2148986823.jpg?w=740&t=st=1706722771~exp=1706723371~hmac=1e651702e0c603309695588127f6d04f3cad1f952cfafa3c83a37b494c25f579" alt="logo" /></div>
+      <div className="links">
+         <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>cart</li>
+         </ul>
+      </div>
+    </div>
+  )
+}
+
+
+
+const RestaurentCard = () =>{
+  // const {name,cloudinaryImageId,cuisines} = resData;
+  // console.log(name)
+  console.log(resData);
+  return(
+    <div className="cardContainer" >
+      {resData.map(restaurant => <div key={restaurant.info.id}>
+        <h3>{restaurant.info.name}</h3>
+        <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/" + restaurant.info.cloudinaryImageId} alt="foodspic"/>
+        <h4>{restaurant.info.cuisines.join(",")}</h4>
+        <h4>{restaurant.info.avgRating}</h4>
+        <h5>{restaurant.info.costForTwo}</h5>
+      </div>)}
+    </div>
+  )
+}
+
+const Body = () =>{
+  return (
+    <div className="body">
+       <div className="search">Search</div>
+       <div className="resCard">
+              <RestaurentCard  />
+        </div>
+    </div>
+  )
+}
+
+
+const AppLayout = ()=>{
+  return(
+    <div>
+        <Header />
+        <Body />
+    </div>
+  )
+  }
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout  />)
