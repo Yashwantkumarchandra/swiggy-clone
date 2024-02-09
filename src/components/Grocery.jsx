5ac507e147d07334b5864e3c6422abba2b3c3113
yashwantkumarchandra <yashwantchandra904@gmail.com>
2024-02-10 02:00:34 +0530

    making Lazy or chunking component

---
+++ src/components/Grocery.jsx
@@ -0,0 +1,10 @@
+const Grocery = () => {
+  return (
+    <div>
+      <h1>This is our grocery component & It has lots of child component</h1>
+      <p>like- Items, foods,Delivery time, DOM , DOE</p>
+    </div>
+  );
+};
+
+export default Grocery;
